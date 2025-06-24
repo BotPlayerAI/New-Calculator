ee4c0abc9a3572f8e84cfd3134d2a9368bc1b593
BotPlayerAI <118127221+BotPlayerAI@users.noreply.github.com>
2025-06-24 08:26:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+import addDecimal from "./src/addDecimal.js";
+import addNumberValue from "./src/addNumberValue.js";
+import { inputBtns, clearBtn } from "./src/selectors.js";
+import useOperator from "./src/useOperator.js";
+import { resetAll } from "./src/values.js";
+
+inputBtns.forEach((inputBtn) => {
+  if (inputBtn.classList.length === 0) {
+    inputBtn.addEventListener("click", () => addNumberValue(inputBtn.value));
+  } else if (inputBtn.classList.contains("operator")) {
+    inputBtn.addEventListener("click", () => useOperator(inputBtn.value));
+  } else if (inputBtn.classList.contains("decimal")) {
+    inputBtn.addEventListener("click", () => addDecimal());
+  }
+});
+
+clearBtn.addEventListener("click", resetAll);
---
+++ src/addNumberValue.js
@@ -0,0 +1,15 @@
+import { getAwaitingNextValue, setAwaitingNextValue } from "./values.js";
+import { calculatorDisplay } from "./selectors.js";
+
+function addNumberValue(number) {
+  if (getAwaitingNextValue()) {
+    calculatorDisplay.textContent = number;
+    setAwaitingNextValue(false);
+  } else {
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent =
+      displayValue === "0" ? number : displayValue + number;
+  }
+}
+
+export default addNumberValue;
